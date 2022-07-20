38a32ff4f3b2a6f7bac45f65757930a8ec37497c
iqbalpa <iqbalpahlevi16@gmail.com>
2022-07-20 19:29:52 +0700

    add data.json that contain title and repo url for each frontend mentor challenge

--- src/components/Projects/FEMentor.js
+++ src/components/Projects/FEMentor.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const FEMentor = () => {
     return (
-        <main>
-            
+        <main className="bg-MidnightBlue flex flex-col justify-center items-center pt-5 pb-10">
+            <p className="text-2xl font-bold text-green-500 drop-shadow-lg">Frontend Mentor Challenge</p>
         </main>
     )
 }
