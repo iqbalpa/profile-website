4aec9793a69312d65780990e20e170797abffa76
iqbalpa <iqbalpahlevi16@gmail.com>
2022-07-21 07:43:33 +0700

    add id in data.json, created the main structure of Card.js, looping Card component in FEMentor.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const Card = ({ title, url, imagePath }) => {
     return(
-        <main>
-
+        <main className="w-[25rem] bg-Sapphire flex flex-col p-5 rounded-lg border-[1px] border-ResolutionBlue">
+            <img src={imagePath} alt={title} className="rounded-xl w-96 mb-3" />
+            <a href={url} target="_blank" rel="noreferrer" className="font-bold text-lg text-center">{title}</a>
         </main>
     )
 }
--- src/components/Projects/FEMentor.js
+++ src/components/Projects/FEMentor.js
@@ -8,7 +8,7 @@ const FEMentor = () => {
     useEffect(() => {
         fetch("/assets/data.json")
             .then(res => res.json())
-            .then(data => setData(data.frontendMentor))
+            .then(dat => setData(dat))
             .catch(err => console.log(err));
     }, []);
 
@@ -16,11 +16,12 @@ const FEMentor = () => {
         <main className="bg-MidnightBlue flex flex-col justify-center items-center pt-5 pb-10">
             <p className="text-2xl font-bold text-green-500 drop-shadow-lg">Frontend Mentor Challenge</p>
 
-            {/* create loop for the data */}
-            {data.map(item => (
-                <Card key={item.id} title={item.title} url={item.url} imagePath={item.imagePath} />
-            ))}
-
+            <div className="grid grid-cols-3 grid-rows-4">
+                {/* create loop for the data */}
+                {data.map(item => (
+                    <Card key={item.id} title={item.title} url={item.url} imagePath={item.imagePath} />
+                ))}
+            </div>
         </main>
     )
 }
