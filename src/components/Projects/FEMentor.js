0eba1737bad45bae39bff8882323d621c6d13b54
iqbalpa <iqbalpahlevi16@gmail.com>
2022-07-22 08:12:11 +0700

    moved the data.json into public folder

--- src/components/Projects/FEMentor.js
+++ src/components/Projects/FEMentor.js
@@ -6,21 +6,24 @@ const FEMentor = () => {
 
     // get data from src/assets/data.json using useeffect
     useEffect(() => {
-        fetch("./../../assets/data.json")
+        fetch("/data.json")
             .then(res => res.json())
-            .then(dat => setData(dat))
-            .catch(() => console.log("fetching data failed"));
+            .then(dat => setData(dat.frontendMentor))
+            .catch(() => console.log("fetching data failed"))
+            .finally(() => console.log(data.length));
     }, []);
 
     return (
         <main className="bg-MidnightBlue flex flex-col justify-center items-center pt-5 pb-10">
             <p className="text-2xl font-bold text-green-500 drop-shadow-lg">Frontend Mentor Challenge</p>
 
-            <div className="grid grid-cols-3 grid-rows-4">
-                {data.map(item => (
-                    <Card key={item.id} title={item.title} url={item.url} imagePath={item.imagePath} />
-                ))}
-            </div>
+            {data && 
+                <div className="grid grid-cols-3 grid-rows-4 gap-5">
+                    {data.map(item => (
+                        <Card key={item.id} title={item.title} url={item.url} imagePath={item.imagePath} />
+                    ))}
+                </div>
+            }
         </main>
     )
 }
