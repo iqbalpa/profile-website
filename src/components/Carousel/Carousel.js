81526dabb8d6eda3d4b80563211ab92ac7c12c03
iqbalpa <iqbalpahlevi16@gmail.com>
2022-07-24 10:33:13 +0700

    finished the simple carousel in Carousel.js

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { GrNext, GrPrevious } from 'react-icons/gr';
 
 const Carousel = ({ listData }) => {
     const images = listData.map(item => item.imagePath);
     const titles = listData.map(item => item.title);
+    const urls = listData.map(item => item.url);
 
     const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -25,14 +26,14 @@ const Carousel = ({ listData }) => {
     return (
         <main className="w-[37rem] m-auto">
             <div className="w-full relative select-none">
-                <div className="bg-red-300 p-4 rounded-xl flex flex-col items-center justify-center">
-                    <img src={images[currentIndex]} alt="" />
-                    <p>{titles[currentIndex]}</p>
+                <div className="bg-Sapphire p-4 rounded-xl flex flex-col items-center justify-center border-[1px] border-ResolutionBlue">
+                    <img src={images[currentIndex]} alt="" className="rounded-2xl" />
+                    <a href={urls[currentIndex]} target="_blank" rel="noreferrer" className="mt-4 text-lg font-semibold text-white hover:text-gray-400">{titles[currentIndex]}</a>
                 </div>
 
-                <div className="absolute w-full top-1/2 transform -translate-y-1/2 flex justify-between items-start px-5">
-                    <button onClick={handlePrevClick}><GrPrevious /></button>
-                    <button onClick={handleNextClick}><GrNext /></button>
+                <div className="absolute w-full top-1/2 transform -translate-y-1/2 flex justify-between items-start px-7">
+                    <button onClick={handlePrevClick} className="bg-slate-300/75 hover:bg-slate-500/75 p-2 rounded-full"><GrPrevious /></button>
+                    <button onClick={handleNextClick} className="bg-slate-300/75 hover:bg-slate-500/75 p-2 rounded-full"><GrNext /></button>
                 </div>
             </div>
         </main> 
