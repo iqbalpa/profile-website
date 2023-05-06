e6845849244358bd548ab8dd9516f9f4dc10d839
iqbalpa <iqbalpahlevi16@gmail.com>
2023-05-06 10:42:23 +0700

    feat: add logo to header & delete deprecated components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
-import Project from "./components/Projects/Project"
-import FEMentor from "./components/Projects/FEMentor"
 
 export default function App() {
   return (
     <>
       <Header />
       <Hero />
-      <Project />
-      <FEMentor />
     </>
   )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,24 @@
 import React from "react";
+import Logo from "../../assets/logo-1.png";
 
 const Header = () => {
-    return (
-        <header className="fixed w-screen h-16 z-10 flex flex-row justify-between items-center bg-ResolutionBlue text-white">
-            <a href="#" className="ml-8 text-xl font-extrabold">IQBAL</a>
+	return (
+		<header className="fixed w-screen h-[4.5rem] z-10 flex flex-row justify-between items-center bg-ResolutionBlue text-white">
+			<img src={Logo} alt="logo" className="w-32 h-10 ml-5" />
 
-            <div className="flex flex-row items-center pr-6 text-md font-semibold">
-                <a href="#1" className="m-3 duration-75 hover:text-green-500">About</a>
-                <a href="#2" className="m-3 duration-75 hover:text-green-500">Projects</a>
-                <a href="mailto:iqbalpahlevi07@gmail.com">
-                    <button className="hover:scale-105 m-3 p-2 rounded-lg border-2 border-green-500 duration-300 hover:bg-green-500 text-green-500 hover:text-ResolutionBlue">Contact Me</button>
-                </a>
-            </div>
-        </header>
-    );
-}
+			<div className="flex flex-row items-center mr-8 text-md font-semibold">
+				<a href="#1" className="text-xl m-3 duration-75 hover:text-green-500">
+					About
+				</a>
+				<a href="#2" className="text-xl m-3 duration-75 hover:text-green-500">
+					Projects
+				</a>
+				<a href="mailto:iqbalpahlevi07@gmail.com">
+					<button className="hover:scale-105 m-3 p-2 text-lg rounded-lg border-2 border-green-500 duration-300 hover:bg-green-500 text-green-500 hover:text-ResolutionBlue">Contact Me</button>
+				</a>
+			</div>
+		</header>
+	);
+};
 
 export default Header;
