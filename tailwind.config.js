46e9601d7d673aef874f7c9a45f6a121f5399074
iqbalpa <iqbalpahlevi16@gmail.com>
2023-05-06 10:44:56 +0700

    config: daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors:{
-        'Sapphire':'#0C023E',
-        'MidnightBlue':'#1D2570',
-        'ResolutionBlue':'#2F387B',
-      }
-    },
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				Sapphire: "#0C023E",
+				MidnightBlue: "#1D2570",
+				ResolutionBlue: "#2F387B",
+			},
+		},
+	},
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: ["light"],
+	},
+};
