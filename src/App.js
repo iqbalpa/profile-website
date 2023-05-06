84b61b434164a5c04dc24dcbd8b4b22df4992f89
iqbalpa <iqbalpahlevi16@gmail.com>
2023-05-06 12:00:51 +0700

    feat: create project section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Award from "./components/Award/Award"
-import Header from "./components/Header/Header"
-import Hero from "./components/Hero/Hero"
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Project from "./components/Project/Project";
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <Award />
-    </>
-  )
+	return (
+		<>
+			<Header />
+			<Hero />
+			<Project />
+		</>
+	);
 }
