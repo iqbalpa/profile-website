bc1fd3fc7e38862070f08a0c7cf76427284ccd2e
iqbalpa <iqbalpahlevi16@gmail.com>
2023-05-06 16:57:12 +0700

    refactor: project card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Card = ({ imageLink, title, description, link }) => {
+	return (
+		<>
+			<div className="card card-compact w-96 bg-base-100 shadow-xl">
+				<figure>
+					<img src={imageLink} alt="Numbers API" />
+				</figure>
+				<div className="card-body">
+					<h2 className="card-title">{title}</h2>
+					<p>{description}</p>
+					<div className="card-actions justify-end">
+						<a href={link} target="_blank" rel="noreferrer">
+							<button className="btn btn-success hover:scale-105">check it out</button>
+						</a>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Card;
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,72 +1,49 @@
 import React from "react";
+import Card from "../Card/Card";
 import Netflix from "../../assets/netflix-clone.png";
 import FaceDetector from "../../assets/face-detector.png";
 import ParisHousing from "../../assets/paris-housing-price.png";
 import NumbersApi from "../../assets/numbers-api.png";
 
 const Project = () => {
+	const data = {
+		projects: [
+			{
+				imageLink: Netflix,
+				title: "Netflix Website clone",
+				description: "Netflix Website interface clone using React JS and Tailwind CSS",
+				link: "https://netflix-clone-iqbalpa.vercel.app/",
+			},
+			{
+				imageLink: FaceDetector,
+				title: "Face Detector and Finger Counter",
+				description: "Face detector and finger counter based on python opencv library",
+				link: "https://github.com/iqbalpa/opencv",
+			},
+			{
+				imageLink: ParisHousing,
+				title: "Paris Housing Price Prediction",
+				description: "Kaggle Competition deployed machine learning model into website using Streamlit",
+				link: "https://iqbalpa-final-project-educare-app-aq96jt.streamlit.app/",
+			},
+			{
+				imageLink: NumbersApi,
+				title: "Numbers Random Fact",
+				description: "Generate random fact of a number using numbersapi.com",
+				link: "https://iqbalpa-numbers-api.netlify.app/",
+			},
+		],
+	};
+
 	return (
 		<>
 			<div id="2" className="flex flex-col justify-center items-center bg-ResolutionBlue py-14">
 				<h1 className="text-2xl text-green-500 font-bold">P R O J E C T</h1>
 				<p className="text-lg">Here is the project I have worked with lately</p>
 				<div className="grid grid-cols-3 gap-14 mt-8">
-					<div className="card card-compact w-96 bg-base-100 shadow-xl">
-						<figure>
-							<img src={Netflix} alt="Netflix Clone" />
-						</figure>
-						<div className="card-body">
-							<h2 className="card-title">Netflix Website clone</h2>
-							<p>Netflix Website interface clone using React JS and Tailwind CSS</p>
-							<div className="card-actions justify-end">
-								<a href="https://netflix-clone-iqbalpa.vercel.app/" target="_blank" rel="noreferrer">
-									<button className="btn btn-success hover:scale-105">check it out</button>
-								</a>
-							</div>
-						</div>
-					</div>
-					<div className="card card-compact w-96 bg-base-100 shadow-xl">
-						<figure>
-							<img src={FaceDetector} alt="Face Detector" />
-						</figure>
-						<div className="card-body">
-							<h2 className="card-title">Face Detector and Finger Counter</h2>
-							<p>Face detector and finger counter based on python opencv library</p>
-							<div className="card-actions justify-end">
-								<a href="https://github.com/iqbalpa/opencv" target="_blank" rel="noreferrer">
-									<button className="btn btn-success hover:scale-105">check it out</button>
-								</a>
-							</div>
-						</div>
-					</div>
-					<div className="card card-compact w-96 bg-base-100 shadow-xl">
-						<figure>
-							<img src={ParisHousing} alt="Paris Housing Price Predictor" />
-						</figure>
-						<div className="card-body">
-							<h2 className="card-title">Paris Housing Price Prediction</h2>
-							<p>Kaggle Competition deployed machine learning model into website using Streamlit</p>
-							<div className="card-actions justify-end">
-								<a href="https://iqbalpa-final-project-educare-app-aq96jt.streamlit.app/" target="_blank" rel="noreferrer">
-									<button className="btn btn-success hover:scale-105">check it out</button>
-								</a>
-							</div>
-						</div>
-					</div>
-					<div className="card card-compact w-96 bg-base-100 shadow-xl">
-						<figure>
-							<img src={NumbersApi} alt="Numbers API" />
-						</figure>
-						<div className="card-body">
-							<h2 className="card-title">Numbers Random Fact</h2>
-							<p>Generate random fact of a number using numbersapi.com</p>
-							<div className="card-actions justify-end">
-								<a href="https://iqbalpa-numbers-api.vercel.app/" target="_blank" rel="noreferrer">
-									<button className="btn btn-success hover:scale-105">check it out</button>
-								</a>
-							</div>
-						</div>
-					</div>
+					{data.projects.map((project, index) => (
+						<Card key={index} imageLink={project.imageLink} title={project.title} description={project.description} link={project.link} />
+					))}
 				</div>
 			</div>
 		</>
