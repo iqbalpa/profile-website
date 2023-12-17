8d9e8185f35f12ceb6ec7e99b2bd56f57b122c5d
iqbalpa <iqbalpahlevi16@gmail.com>
2023-12-18 05:23:14 +0700

    feat: update experiences

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -7,7 +7,7 @@ const Experience = () => {
 			<div class="bg-ResolutionBlue text-white flex flex-col justify-center py-6 lg:py-14">
 				<Fade>
 					<h1 className="text-center text-green-500 text-2xl font-bold">E X P E R I E N C E</h1>
-					<p className="text-center text-lg mb-5 text-slate-300">Here is my experiences</p>
+					<p className="text-center text-lg mb-5 text-slate-300">These are some of my experiences in tech</p>
 					<div class="w-full mx-auto lg:max-w-4xl">
 						<div class="relative">
 							<div class="absolute hidden w-px h-full transform -translate-x-1/2 bg-indigo-300 lg:block left-1/2"></div>
@@ -18,20 +18,46 @@ const Experience = () => {
 											<div class="flex items-center justify-end w-full mx-auto">
 												<div class="w-full lg:pr-8">
 													<div class="w-full text-xl text-right">
-														<h1 className="font-bold">Fasilkom UI</h1>
-														<h2 className="text-lg">January 2023 - Present</h2>
+														<h1 className="font-bold">
+															Japan Advanced Institute of Science and Technology (JAIST)
+														</h1>
+														<h2 className="text-lg">November 2023 - Present</h2>
 													</div>
 												</div>
 											</div>
 											<div class="flex items-start justify-start w-full mx-auto">
 												<div class="w-full lg:pl-8 text-left">
-													<p className="text-xl">Teaching Assistant for Programming Foundations 1</p>
+													<p className="text-xl">Research Intern for speech translation</p>
 												</div>
 											</div>
 										</div>
 										<div class="absolute flex items-center justify-center w-4 h-4 transform -translate-x-1/2 -translate-y-4 bg-green-500 border-2 border-green-500 rounded-full left-1/2 sm:translate-y-0"></div>
 									</div>
 								</div>
+
+								<div>
+									<div class="flex flex-col items-center">
+										<div className="w-[40rem] flex flex-row">
+											<div class="flex items-center justify-end w-full mx-auto">
+												<div class="w-full lg:pr-8">
+													<div class="w-full text-xl text-right">
+														<h1 className="font-bold">Faculty of Computer Science UI</h1>
+														<h2 className="text-lg">August 2023 - Present</h2>
+													</div>
+												</div>
+											</div>
+											<div class="flex items-start justify-start w-full mx-auto">
+												<div class="w-full lg:pl-8 text-left">
+													<p className="text-xl">
+														Teaching Assistant for Introduction to AI & Data Science
+													</p>
+												</div>
+											</div>
+										</div>
+										<div class="absolute flex items-center justify-center w-4 h-4 transform -translate-x-1/2 -translate-y-4 bg-green-500 border-2 border-green-500 rounded-full left-1/2 sm:translate-y-0"></div>
+									</div>
+								</div>
+
 								<div>
 									<div class="flex flex-col items-center">
 										<div className="w-[40rem] flex flex-row">
@@ -80,14 +106,16 @@ const Experience = () => {
 											<div class="flex items-center justify-end w-full mx-auto">
 												<div class="w-full lg:pr-8">
 													<div class="w-full text-xl text-right">
-														<h1 className="font-bold">Fasilkom UI</h1>
-														<h2 className="text-lg">August 2022 - January 2023</h2>
+														<h1 className="font-bold">Faculty of Computer Science UI</h1>
+														<h2 className="text-lg">January 2023 - June 2023</h2>
 													</div>
 												</div>
 											</div>
 											<div class="flex items-start justify-start w-full mx-auto">
 												<div class="w-full lg:pl-8 text-left">
-													<p className="text-xl">Teaching Assistant for Calculus 1</p>
+													<p className="text-xl">
+														Teaching Assistant for Programming Foundations 1
+													</p>
 												</div>
 											</div>
 										</div>
@@ -101,14 +129,14 @@ const Experience = () => {
 											<div class="flex items-center justify-end w-full mx-auto">
 												<div class="w-full lg:pr-8">
 													<div class="w-full text-xl text-right">
-														<h1 className="font-bold">Skill Up</h1>
-														<h2 className="text-lg">November 2021 - April 2022</h2>
+														<h1 className="font-bold">Faculty of Computer Science UI</h1>
+														<h2 className="text-lg">August 2022 - January 2023</h2>
 													</div>
 												</div>
 											</div>
 											<div class="flex items-start justify-start w-full mx-auto">
 												<div class="w-full lg:pl-8 text-left">
-													<p className="text-xl">Technology Apprentice</p>
+													<p className="text-xl">Teaching Assistant for Calculus 1</p>
 												</div>
 											</div>
 										</div>
