aef114264a99e99b4f8dc00843099c759603f136
iqbalpa <iqbalpahlevi16@gmail.com>
2023-12-19 07:53:50 +0700

    feat: add notion api config

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Project from "./components/Project/Project";
+import { queryDatabase } from "../src/api/queryDb";
+import { parseProperties, Post } from "../src/utils/parseProperties";
 
 export default function App() {
 	return (
@@ -14,4 +16,14 @@ export default function App() {
 			<Footer />
 		</>
 	);
+}
+
+export async function getStaticProps() {
+	const database = await queryDatabase();
+	const properties = parseProperties(database);
+	return {
+		props: {
+			properties,
+		},
+	};
 }
---
+++ src/api/client.js
@@ -0,0 +1,7 @@
+import { Client } from "@notionhq/client";
+
+const NOTION_API_KEY = process.env.NOTION_API_KEY ?? "";
+
+export const notion = new Client({
+	auth: NOTION_API_KEY,
+});
